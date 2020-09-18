3d6a0764595d443d580fc42c365e5cd29e7878e0
Praney Behl <praneybehl@gmail.com>
2020-09-19 08:11:24 +1000

    chore(layouts): add a default layout for pages

---
+++ src/layouts/default.tsx
@@ -0,0 +1,28 @@
+import React, {FC} from 'react';
+import Header, {HeaderProps} from '../blocks/Header';
+import Footer from '../blocks/Footer';
+import ScreenReaderOnly from '../components/ScreenReaderOnly';
+import {Main} from "./styles";
+
+export interface DefaultLayoutProps {
+	headerProps?: HeaderProps;
+}
+const DefaultLayout: FC<DefaultLayoutProps> = ({headerProps, children}) => {
+	return (
+		<>
+			<ScreenReaderOnly href="#mainContent">
+				Skip to main content
+			</ScreenReaderOnly>
+			<Header {...headerProps} />
+			<Main id="mainContent">
+				{children}
+			</Main>
+			<Footer/>
+		</>
+	)
+};
+
+/**
+ * @component
+ */
+export default DefaultLayout;
---
+++ src/layouts/styles.tsx
@@ -0,0 +1,13 @@
+// Main content container,
+// off-set main element by header height
+import styled from "styled-components/macro";
+
+export const Main = styled.main`
+	align-items: center;
+	display: flex;
+	flex-direction: column;
+	flex-grow: 1;
+	justify-content: center;
+	margin-top: 62px;
+	text-align: center;
+`;
