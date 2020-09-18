7e6e032aef079e934d0875543590110ab2b1c4bb
Praney Behl <praneybehl@gmail.com>
2020-09-19 08:30:10 +1000

    chore(router): add application routing
    also setup code splitting for page level

---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Router} from "@reach/router";
+import Loader from "../components/Loader";
+
+const HomePage = React.lazy(() => import('../pages/Home'));
+
+const AppRoutes = () => {
+	return (
+		<React.Suspense fallback={<Loader/>}>
+			<Router>
+				<HomePage path="/"/>
+			</Router>
+		</React.Suspense>
+	)
+};
+
+export default AppRoutes;
