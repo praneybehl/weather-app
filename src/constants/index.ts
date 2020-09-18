82af75b875eb618294a5b1084c57faaca1f2d039
Praney Behl <praneybehl@gmail.com>
2020-09-19 08:26:06 +1000

    chore: setup environment variables and constants

---
+++ src/constants/index.ts
@@ -0,0 +1,42 @@
+// Constants
+export const WEATHER_API_ENDPOINT = process.env.REACT_APP_WEATHER_API_ENDPOINT;
+
+export const OPEN_WEATHER_API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
+
+console.log(WEATHER_API_ENDPOINT, OPEN_WEATHER_API_KEY)
+
+export const MAJOR_CITY_CODES = [
+	2147714, // Sydney
+	2158177, // Melbourne
+	2073124, // Darwin
+	2174003, // Brisbane
+	2063523, // Perth
+	2078025, // Adelaide
+	2172517, // Canberra
+	2163355, // Hobart
+];
+
+export const DAYS_OF_WEEK = [
+	'Sunday',
+	'Monday',
+	'Tuesday',
+	'Wednesday',
+	'Thursday',
+	'Friday',
+	'Saturday'
+];
+
+export const MONTHS_OF_YEAR = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December'
+];
