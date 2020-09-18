5fcc80da5fe23e175e9e865ddbe58dfc68b374dc
Praney Behl <praneybehl@gmail.com>
2020-09-19 07:45:48 +1000

    chore: add global typescript typings for css prop and theme prop

---
+++ src/typings/styled.d.ts
@@ -0,0 +1,9 @@
+// import original module declarations
+import 'styled-components'
+import theme from "../theme";
+
+type CustomTheme = typeof theme;
+// and extend them!
+declare module 'styled-components' {
+	export interface DefaultTheme extends CustomTheme {}
+}
