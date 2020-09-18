d2c1d31c3884639a21f5983917c4371a2953abb2
Praney Behl <praneybehl@gmail.com>
2020-09-19 08:26:58 +1000

    chore(components): add Loader component for Loading animation

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import RLoader from 'react-loader-spinner';
+import {LoaderContainer} from "./styles";
+
+const Loader = () => {
+	return (
+		<LoaderContainer>
+			<RLoader
+				type="Bars"
+				color="#e4573b"
+				height={80}
+				width={80}
+			/>
+		</LoaderContainer>
+	)
+};
+
+export default Loader;
