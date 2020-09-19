05d80008cb26de2290e4575f60b9f9b2150644ea
Praney Behl <praneybehl@gmail.com>
2020-09-19 13:37:30 +1000

    chore(routes): add City weather route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,14 @@ import { Router } from '@reach/router';
 import Loader from '../components/Loader';
 
 const HomePage = React.lazy(() => import('../pages/Home'));
+const CityWeather = React.lazy(() => import('../pages/CityWeather'));
 
 const AppRoutes = () => {
 	return (
 		<React.Suspense fallback={<Loader />}>
 			<Router>
 				<HomePage path="/" />
+				<CityWeather path="/city/:cityId/:cityName" />
 			</Router>
 		</React.Suspense>
 	);
