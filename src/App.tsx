156245f22eaeadc59787ea5c0a5363dbb331721d
Praney Behl <praneybehl@gmail.com>
2020-09-19 12:02:02 +1000

    fix: update App code styling and liniting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
+import { GlobalStyles } from './theme/global';
+import ScreenReaderOnly from './components/ScreenReaderOnly';
+import AppRoutes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const AppStyled = styled.div`
+	display: flex;
+	flex-direction: column;
+	height: 100vh;
+`;
+
+const App = () => {
+	return (
+		<ThemeProvider theme={theme}>
+			<AppStyled>
+				<ScreenReaderOnly href="#mainContent">Skip to main content</ScreenReaderOnly>
+				<AppRoutes />
+				<GlobalStyles />
+			</AppStyled>
+		</ThemeProvider>
+	);
+};
 
 export default App;
