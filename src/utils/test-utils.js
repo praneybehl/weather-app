0862948442dc193271e09f584221d22a0911324c
Praney Behl <praneybehl@gmail.com>
2020-09-20 21:35:11 +1000

    tests: wrap ThemeSwitcherContext for test customRender

--- src/utils/test-utils.js
+++ src/utils/test-utils.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components/macro';
 import theme from '../theme';
+import { getTheme, modes, ThemeSwitcherContext } from '../theme/useThemeSwitcher';
 
 const AllTheProviders = ({ children }) => {
-	return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+	const [mode, setMode] = useState(modes[0]);
+	return (
+		<ThemeSwitcherContext.Provider value={{ mode, setMode }}>
+			<ThemeProvider theme={getTheme(mode)}>{children}</ThemeProvider>
+		</ThemeSwitcherContext.Provider>
+	);
 };
 
 const customRender = (ui, options) => render(ui, { wrapper: AllTheProviders, ...options });
